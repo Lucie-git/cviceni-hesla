de59a2a8409a152d2c3a9cbe58273651538a583c
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-04-04 17:04:31 +0200

    vyřešeno

--- index.js
+++ index.js
@@ -27,3 +27,19 @@ const strongPassword = (len) => {
 
   return result;
 };
+
+// Napište funkci createAccount, která se bude tvářit, že zakládá nový uživatelský účet.
+// Funkce bude mít dva parametry user a generatePassword.
+// První bude uživatelské jméno a druhý bude funkce, pomocí které se má vygenerovat heslo pro tento účet.
+// Ve funkci createAccount si do proměnné password uložte heslo vygenerované voláním funkce generatePassword.
+// Funkci generatePassword při volání předejte číslo 9 jako délku požadovaného hesla.
+// Funkce createAccount nakonec vrátí řetězec, který bude obsahovat jméno uživatele a vygenerované heslo z proměnné password.
+// Například vrátí "Uživateli Míša s heslem 012345678".
+
+const createAccount = (user, generatePassword) => {
+  const userName = prompt('Zadejte uživatelské jméno:');
+  const password = strongPassword(9);
+  return `User name: ${userName} <br> password: ${password}`;
+};
+
+document.body.innerHTML += createAccount();
